d6cb84ce44c8097ec89db6dfaab5ea8ada54b778
tduboudi <thomas.duboudin@gmail.com>
2017-04-28 22:10:55 +0200

    Add files via upload
    modifs weex + kofu

--- BE_Fourmi2.py
+++ BE_Fourmi2.py
@@ -32,8 +32,7 @@ class Route :
 
 class Ville :
     
-    def __init__(self, nom, X, Y):
-        self.nom = nom
+    def __init__(self, X, Y):
         self.X = X
         self.Y = Y
      
@@ -141,12 +140,12 @@ class Fourmi :
 
 class Civilisation :
     
-    def __init__(self, routes = [(0,1), (1,2), (1,3), (2,3), (3,4), (2,4)], villes=[('nid', 0,0), ('ville1', 20, 20), ('ville2', 70, 30), ('ville3', 50, 80), ('food', 100, 50)]):
-        self.villes = [Ville(v[0], v[1], v[2]) for v in villes]
+    def __init__(self, routes = [(0,1), (1,2), (1,3), (2,3), (3,4), (2,4)], villes=[('nid', 0,0), ('ville1', 20, 20), ('ville2', 70, 30), ('ville3', 50, 80), ('food', 100, 50)], nb_fourmis=20):
+        self.villes = [Ville(v[0], v[1]) for v in villes]
         self.ville_nid = self.villes[0]
         self.ville_food = self.villes[-1]
         self.routes = [Route(self.villes[r[0]], self.villes[r[1]]) for r in routes]
-        self.fourmis = [Fourmi(rand.random(), 10*rand.random(), 5*rand.random(), self.ville_nid.get_position(),  self.ville_nid, self.routes[0]) for i in range(20)]
+        self.fourmis = [Fourmi(rand.random(), 10*rand.random(), 5*rand.random(), self.ville_nid.get_position(),  self.ville_nid, self.routes[0]) for i in range(nb_fourmis)]
         self.instant = 1
         # début des mutations après l'instant t=100
     
@@ -256,6 +255,11 @@ class Civilisation :
             ant.reset_nourriture()
             ant.reset_memoire()
 
+    def get_ants_position(self):
+        f = []
+        for ant in self.fourmis:
+            f.append(ant.getpos())
+        return f
 
 def traitement():
     '''
